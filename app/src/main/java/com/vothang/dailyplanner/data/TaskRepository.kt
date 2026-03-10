3a10bb3eeb7ad04cf9c20de0af79bbde4558911f
vothang2k3 <bachduongn244@gmail.com>
2026-03-10 19:33:24 +0700

    feat: add UiState sealed class and update Task model

--- app/src/main/java/com/vothang/dailyplanner/data/TaskRepository.kt
+++ app/src/main/java/com/vothang/dailyplanner/data/TaskRepository.kt
@@ -1,10 +1,14 @@
 package com.vothang.dailyplanner.data
 
 import com.vothang.dailyplanner.model.Task
+import kotlinx.coroutines.flow.Flow
 
 interface TaskRepository {
-    fun getTasks() : List<Task>
-    fun insertTask(task: Task)
-    fun updateTask(task: Task)
-    fun deleteTask(taskId: Int)
+    // Trả về Flow - Tự động cập nhật khi DB thay đổi
+    fun getTasks() : Flow<List<Task>>
+
+    // Suspend function - chạy bất đồng bộ
+    suspend fun insertTask(task: Task)
+    suspend fun updateTask(task: Task)
+    suspend fun deleteTask(taskId: Int)
 }
--- app/src/main/java/com/vothang/dailyplanner/model/Task.kt
+++ app/src/main/java/com/vothang/dailyplanner/model/Task.kt
@@ -3,5 +3,8 @@ package com.vothang.dailyplanner.model
 data class Task(
     val id: Int,
     val title: String,
-    val isDone: Boolean
+    val isDone: Boolean,
+    val note : String,
+    val deadline: Long?,
+    val listId: Int
 )
